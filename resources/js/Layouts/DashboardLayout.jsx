88ceb6ad36f62aab291216b2168661d0084d1389
kefayetur rahman <kefayeturrahman492@gmail.com>
2024-09-25 10:09:02 +0600

    dashboard leftsidebar has been done

--- resources/js/Layouts/DashboardLayout.jsx
+++ resources/js/Layouts/DashboardLayout.jsx
@@ -1,18 +1,35 @@
 import DashboardNavBar from "@/Components/dashboard/DashboardNavBar";
 import LeftSideBar from "@/Components/dashboard/LeftSideBar";
 import { DashboardActionContext } from "@/Context/DashboardActionContext";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 
 const DashboardLayout = ({ children }) => {
+    const SideBarRef = useRef(null);
     const { showSidebar, setShowSideBar } = useContext(DashboardActionContext);
+
+    const handleSidebar = (e) => {
+        if (SideBarRef.current && !SideBarRef.current.contains(e.target)) {
+            console.log("helle event");
+            setShowSideBar(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleSidebar, true);
+
+        () => document.removeEventListener("click", handleSidebar, true);
+    }, []);
 
     return (
         <>
             <DashboardNavBar />
-            <div className="h-screen">
+            <div className={`h-screen ${showSidebar ? "bg-slate-400" : ""}`}>
                 <div className="flex">
                     <div
-                        className={`left pr-4  lg:w-[20%]  md:w-[35%] absolute top-0 hidden lg:block ${showSidebar ? "md:block" : ""}`}
+                        className={`left pr-4  lg:w-[20%]  md:w-[35%] absolute top-0 hidden lg:block ${
+                            showSidebar ? "md:block " : ""
+                        }`}
+                        ref={SideBarRef}
                     >
                         <LeftSideBar />
                     </div>
